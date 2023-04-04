983e8790335cd7b70388d47ebf9f4a42e57b9027
smmartprogrammer <partabrajp@gmail.com>
2023-04-04 15:20:31 +0500

    projects

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -12,7 +12,7 @@ function Skill({ directionLeft }: Props) {
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
         src="typescript.png"
-        className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32  filter group-hover:grayscale transition duration-300 ease-in-out"
+        className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
         alt=""
       />
       <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0 ">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Hero from "../../components/Hero";
 import About from "../../components/About";
 import WorkExperience from "../../components/WorkExperience";
 import Skills from "../../components/Skills";
+import Projects from "../../components/Projects";
 
 const Home: NextPage = () => {
   return (
@@ -22,13 +23,14 @@ const Home: NextPage = () => {
       <WorkExperience />      </section>
       <section id="skills" className="snap-center">
       <Skills />      </section>
+      <section id="projects" className="snap-start">
+      <Projects />      </section>
 
 
 
 
-
-{/* <Project /> 
-<Contact Me />  */} */}
+ 
+{/* <Contact Me />   */}
     </div>
   );
 };
